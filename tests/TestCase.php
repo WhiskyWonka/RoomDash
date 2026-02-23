1a3bef50e1c5a0b41477ba4fdbc05c7b5b220d2f
Victor Encolor <dr.victor.o@gmail.com>
2026-02-23 19:35:38 -0300

    Fix .env.example por .env.testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,18 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Http;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // El asterisco al principio captura http, https y cualquier subdominio
+        Http::fake([
+            '*api.pwnedpasswords.com*' => Http::response([], 200),
+            '*' => Http::response([], 200),
+        ]);
+    }
 }
